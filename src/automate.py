0dd05f5da1d2d113ccd68b6f725e802e043e90cf
Ylox <git@ylox.xyz>
2026-06-26 00:38:11 +0200

    GUI Continued and Automate ready to start

---
+++ src/automate.py
@@ -0,0 +1,10 @@
+"""The automation features"""
+
+import time
+import sys
+import pyautogui as pag
+
+pag.FAILSAFE = True # Default but better be safe lol
+
+def auto_forge():
+    pass
--- src/gui.py
+++ src/gui.py
@@ -1,29 +1,9 @@
 """Building the GUI"""
 
 import time
-from PIL import Image
 import customtkinter as ctk
-
-
-class ImageButton(ctk.CTkButton):
-    """Custom Button with Hover Effect"""
-
-    def __init__(self, root, img1, img2, *args, **kwargs):
-        super().__init__(root, *args, **kwargs)
-
-        self.img1 = ctk.CTkImage(Image.open(img1), size=(64, 64))
-        self.img2 = ctk.CTkImage(Image.open(img2), size=(64, 64))
-
-        self.image = self.img1
-
-        self.bind("<Enter>", self.enter)
-        self.bind("<Leave>", self.leave)
-
-    def enter(self, event):
-        self.configure(image=self.img2)
-
-    def leave(self, event):
-        self.configure(image=self.img1)
+from PIL import Image
+from automate import auto_forge
 
 
 class ForgeApp(ctk.CTk):
@@ -32,35 +12,47 @@ class ForgeApp(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        ctk.set_appearance_mode("Dark")
+
         ### Window Basics
         self.title("Pantheon Forge")
         self.iconbitmap("assets/favicon.ico")
 
+        # self.configure(fg_color="#571322")
+        self.bg_image=ctk.CTkImage(Image.open("assets/forge_background.png"), size=(300,300))
+        self.bg_label = ctk.CTkLabel(self,text="", image=self.bg_image)
+        self.bg_label.place(x=0,y=0)
+
         self.geometry("300x200")
         self.resizable(False, False)
+
         ### Widgets
         # Header
         self.headline = ctk.CTkLabel(
             self,
             text="Pantheon Forge",
             font=ctk.CTkFont(family="Arial", size=32, weight="bold"),
+            text_color=("#F2F0EF"),
+            fg_color="#005484",
         )
 
         # Start Button
-        self.start_button = ImageButton(
+        self.start_button = ctk.CTkButton(
             self,
             command=self.start_automation,
-            img1="assets/forge_extinguished.png",
-            img2="assets/forge_upscaled.png",
-            text="",
+            text="START",
+            fg_color="#135748",
+            hover_color="#1f7c6a",
+            text_color="#F2F0EF",
+            font=ctk.CTkFont(family="Arial", size=32, weight="bold"),
         )
 
         # Status label
         self.status_label = ctk.CTkLabel(
             self,
-            text="READY",
+            text="STATUS: READY",
             text_color="black",
-            fg_color="white",
+            fg_color="#F2F0EF",
             font=ctk.CTkFont(family="Arial", size=24, weight="bold"),
             padx=2,
             corner_radius=16,
@@ -72,14 +64,14 @@ class ForgeApp(ctk.CTk):
 
         self.headline.grid(row=0, column=0, padx=16, pady=(16, 8), sticky="ew")
         self.start_button.grid(row=1, column=0, pady=(8, 8))
-        self.status_label.grid(row=2, column=0, pady=(8, 8))
+        self.status_label.grid(row=2, column=0, pady=(8, 16))
 
     def start_automation(self):
         """Runs when the start button is pressed, activates the automation and updates the status label."""
-        self.status_label.configure(text="FORGING", fg_color="orange")
+        self.status_label.configure(text="STATUS: FORGING", fg_color="orange")
         self.update_idletasks()
-        time.sleep(5)
-        self.status_label.configure(text="FINISHED", fg_color="green")
+        auto_forge()
+        self.status_label.configure(text="STATUS: FINISHED", fg_color="green")
 
 
 if __name__ == "__main__":
